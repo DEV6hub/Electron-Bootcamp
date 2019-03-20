70bc451479e5e89cb8055b279b969e33760bd6b4
Traian_Rusu <traian_rusu@ntgd02.newyyz.local>
2019-03-20 15:39:05 -0400

    updated css styles and a little bit of html to make app nicer and closer to wireframes

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -1,37 +1,47 @@
 const axios = require('axios');
 
-window.catalog = window.catalog || {},
-function(n) {
-    catalog.handler = {
-      init: function() {
-        let shirtsContainer = document.querySelector('#shirts-container');
-        axios({
-          method: 'get',
-          url: 'http://localhost:4000/tshirts'
-          })
-          .then(function (response) {
-              //handle success
-              let newHTML = '';
-              for (let i = 0; i < response.data.length; i++) {
-                newHTML += '<div class="card">'
-               + '<img src=' + '".' + response.data[i].imgSrc + '"' + ' alt="TshirtImage" style="width:100%">'
-               + '<h1>' + response.data[i].name + '</h1>'
-               + '<p class="title">' + response.data[i].type + '</p>'
-               + '<p>$' + response.data[i].price +'</p>'
-               + '<p><button>Buy</button></p></div>';
-              }
-              shirtsContainer.innerHTML = newHTML;
-             // shirtsContainer.append(response.data);
-          })
-          .catch(function (response) {
-              //handle error
-              console.log(response);
-          });
-      }
-    };
+(window.catalog = window.catalog || {}),
+	(function(n) {
+		catalog.handler = {
+			init: function() {
+				let shirtsContainer = document.querySelector('#shirts-container');
+				axios({
+					method: 'get',
+					url: 'http://localhost:4000/tshirts'
+				})
+					.then(function(response) {
+						//handle success
+						let newHTML = '';
+						for (let i = 0; i < response.data.length; i++) {
+							newHTML +=
+								'<div class="card"><div class="img-content">' +
+								'<img src=' +
+								'".' +
+								response.data[i].imgSrc +
+								'"' +
+								' alt="TshirtImage" style="width:100%"> </div>' +
+								'<div class="text-content"><h4>' +
+								response.data[i].name +
+								'</h4>' +
+								'<p class="title">' +
+								response.data[i].type +
+								'</p>' +
+								'<p>$' +
+								response.data[i].price +
+								'</p></div>' +
+								'<p><button>Buy</button></p></div>';
+						}
+						shirtsContainer.innerHTML = newHTML;
+						// shirtsContainer.append(response.data);
+					})
+					.catch(function(response) {
+						//handle error
+						console.log(response);
+					});
+			}
+		};
 
-    n(function() {
-      catalog.handler.init();
-    })
-
-}(jQuery);
+		n(function() {
+			catalog.handler.init();
+		});
+	})(jQuery);
